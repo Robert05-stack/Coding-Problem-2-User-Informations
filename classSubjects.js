a08dbe8af4262114d56ebfa784aa4245ab810dce
Robert05-stack <johnrobertabaya@gmail.com>
2024-11-04 19:55:24 +0800

    initial commit

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+// Collecting user information
+const subjectTitle = "Data Structure and Algorithm"; // Replace with actual input
+const classSchedule = "9:30 AM - 11:00 AM, TUE-THU"; // Replace with actual input
+const classroom = "COMLAB A"; // Replace with actual input
+const classInstructor = "Sir. Vj Varayuga"; // Replace with actual input
+const studentName = "John Robert Abaya"; // Replace with actual input
+
+// Logging the information to the console
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}.
